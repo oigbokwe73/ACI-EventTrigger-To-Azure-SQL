b18757240f72626d1f26829204011e349401aeca
obinna <oigbokwe73@gmail.com>
2022-01-19 09:45:38 -0500

    Add project files.

---
+++ AciToSQL/EventTrigger.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core;
+using Xenhey.BPM.Core.Implementation;
+
+namespace AciToSQL
+{
+    public static class EventTrigger
+    {
+        [FunctionName("EventTrigger")]
+        public static void Run([BlobTrigger("processed/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
+        {
+            string ApiKeyName = "x-api-key";
+            log.LogInformation("C# blob trigger function processed a request.");
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add(ApiKeyName, "3FB620B0E0FD4E8F93C9E4D839D00E1D");
+            IOrchrestatorService orchrestatorService = new ManagedOrchestratorService(nvc);
+            var processFiles = orchrestatorService.Run(myBlob);
+        }
+    }
+}
